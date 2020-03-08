7ef3e6f7230a2bf3decf24acb8bf249083627c14
Warren Fisher <github@warrenfisher.dev>
2020-03-08 18:40:02 -0300

    Numbers

--- website/src/index.js
+++ website/src/index.js
@@ -6,15 +6,15 @@ class Square extends React.Component {
     render() {
       return (
         <button className="square">
-          {/* TODO */}
+            {this.props.value}
         </button>
       );
     }
   }
-
+  
   class Board extends React.Component {
     renderSquare(i) {
-      return <Square />;
+      return <Square value={i} />;
     }
   
     render() {
@@ -65,3 +65,4 @@ class Square extends React.Component {
     <Game />,
     document.getElementById('root')
   );
+  
