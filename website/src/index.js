0ffc57e5a10a85241e22212dba4ccc1be24d8ae1
Warren Fisher <github@warrenfisher.dev>
2020-03-16 11:26:33 -0300

    Handle some state of searches

--- website/src/index.js
+++ website/src/index.js
@@ -6,6 +6,9 @@ class Search extends React.Component {
   constructor(props) {
     super(props);
     this.handleChange = this.handleChange.bind(this);
+    this.state = {
+      classes: []
+    }
   }
 
   handleChange(e) {
@@ -13,14 +16,32 @@ class Search extends React.Component {
     if (val !== "") {
       let resp = apiAction(val);
       resp.then(result => {
+        if (result === null) {
+          return
+        }
+        let cls = this.state.classes.slice();
+        cls.push(result);
         console.log(result);
+        this.setState({classes: cls})
       }).catch(() => {console.log("fail")})
     }
   }
 
   render() {
+    const data = this.state.classes
     return (
+      <div id='main'>
       <input type='text' className='input' placeholder='Search...' onChange={this.handleChange}/>
+      <div class="class">
+        <ul>
+          {data.map((d) => {
+              return <li key={d.crn}>{d.name}</li>
+              }
+            )
+          }
+        </ul>
+      </div>
+      </div>
     )
   }
 }
