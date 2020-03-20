681ad3645b8eb0c069295ade463bc8febb3110b7
Warren Fisher <github@warrenfisher.dev>
2020-03-20 15:21:49 -0300

    Time and search query

--- api/sql.py
+++ api/sql.py
@@ -40,6 +40,9 @@ def timedelta_helper(t):
     return f"{hours}:{minutes}"
 
 def name_query(search):
+    """
+    Raw SQL query for if trying to match a string
+    """
     search = '%' + search + '%'
     s = text("""SELECT C_CRN, C_NAME, D_CODE, C_DAYS, C_TIMESTART, C_TIMEEND,
              C_CREDIT_HRS FROM classInfo WHERE C_NAME LIKE :x""")
@@ -47,13 +50,44 @@ def name_query(search):
     return raw_query_helper(result)
 
 def time_query(start_time, end_time):
-    start_time = '11:00:00'
-    end_time = '15:00:00'
+    """
+    Raw SQL query for if trying to match only a start and end time
+    """
+    start_time = time_helper(start_time)
+    end_time = time_helper(end_time)
 
     s = text("""SELECT C_CRN, C_NAME, D_CODE, C_DAYS, C_TIMESTART, C_TIMEEND,
              C_CREDIT_HRS FROM classInfo WHERE C_TIMESTART > :x AND C_TIMEEND < :y""")
     result = engine.connect().execute(s, x=start_time, y=end_time)
     return raw_query_helper(result)
+
+def time_and_search_query(search, start_time, end_time):
+    """
+    Raw SQL query for if trying to match a string and a start and end time
+    """
+    search = '%' + search + '%'
+    start_time = time_helper(start_time)
+    end_time = time_helper(end_time)
+    print(f"{start_time=} {end_time=}")
+
+    s = text("""SELECT C_CRN, C_NAME, D_CODE, C_DAYS, C_TIMESTART, C_TIMEEND,
+            C_CREDIT_HRS FROM classInfo WHERE C_TIMESTART > :x AND C_TIMEEND < :y AND C_NAME LIKE :z""")
+    result = engine.connect().execute(s, x=start_time, y=end_time, z=search)
+    return raw_query_helper(result)
+
+def time_helper(time):
+    """
+    Helper function to format time in the form of HH:MM:SS for MySQL raw query.
+    Expects a user input of HH, H or HHMM
+    """
+    size = len(time)
+    if size == 2:
+        return time + ":00:00"
+    if size == 1:
+        return "0" + time + ":00:00"
+    if size == 4:
+        return time[0:1] + ":" + time[2:3] + ":00"
+    return None
 
 def raw_query_helper(results):
     data = {}
@@ -69,7 +103,6 @@ def raw_query_helper(results):
         }
         data[d['name']] = d
     return data
-
 
 if __name__ == "__main__":
     name_query("Database")
