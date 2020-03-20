03e4dd7f97c0266836d2bbc27c4d91753f88b5c2
Warren Fisher <github@warrenfisher.dev>
2020-03-20 14:59:37 -0300

    Initial state of time searching

--- website/src/index.js
+++ website/src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 
 class ClassInfo extends React.Component {
   renderClass(d) {
-    return <div class="class_" id={d.crn}>CRN={d.crn} dept={d.department} name={d.name}</div>
+    return <div class="class_" id={d.crn}>CRN={d.crn} dept={d.department} name={d.name} start={d.start_time} end={d.end_time}</div>
   }
 
   render() {
@@ -26,41 +26,60 @@ class SearchState extends React.Component {
     }
   }
 
+  apiResponseState(result) {
+    let cls = []
+    if (result === null) {
+      console.log("no results");
+    }
+    let keys = Object.keys(result)
+    for (const name of keys) {
+      cls.push(result[name]);
+    }
+    this.setState({classes: cls})
+  }
+
   stringSearch(val) {
     if (val !== "") {
       let resp = this.getSearch(val);
       resp.then(result => {
-        if (result === null) {
-          return
-        }
-        let cls = []
-        let keys = Object.keys(result)
-        for (const name of keys) {
-          cls.push(result[name]);
-        }
-        // return cls;
-        this.setState({classes: cls})
+        this.apiResponseState(result);
       }).catch(() => {console.log("fail")})
     }
   }
 
-  timeSearch(val) {
+  timeSearch() {
+    // const start = this.state.time-start;
+    // const end = this.state.time-end;
+    const start = 1;
+    const end = 12;
 
+    let resp = this.getTime(start, end);
+    resp.then(result => {
+      this.apiResponseState(result);
+    }).catch(() => {console.log("fail")})
   }
 
   handleChange(e) {
     const target = e.target;
     const name = target.name;
     const val = target.value;
+    this.setState({[name]: val});
 
     console.log(name, val);
     console.log(this.state);
 
     if (name=="string-search") {
-      this.stringSearch(val);
+      let str_cls = this.stringSearch(val);
+    } else if (name=='time-start' || name=='time-end') {
+      this.timeSearch();
     }
+  }
 
-    this.setState({[name]: val});
+  async getTime(start, end) {
+    const response = await fetch(`http://localhost:5000/api/time/${start}/${end}`, {
+      method: 'GET',
+    });
+    return await response.json();
   }
 
   async getSearch(search) {
