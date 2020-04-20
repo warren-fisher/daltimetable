b509c78afb0594a248c1e8bf10a952c236c62326
Warren Fisher <github@warrenfisher.dev>
2020-04-20 16:52:09 -0300

    Display selected classes

--- website/src/other_components.js
+++ website/src/other_components.js
@@ -12,17 +12,28 @@ export function CheckboxDay(props) {
 }
 
 export function ClassInfo(props) {
-  const d = props.data;
-  return (
+    const d = props.data;
+    return (
     <div className="class_" id={d.crn}>
         <input type='checkbox' name={d.crn} onChange={props.handleChange} checked={props.checked}/>
         CRN={d.crn} dept={d.department} name={d.name} start={d.start_time} end={d.end_time} days={d.dates}
     </div>
-  );
+    );
 }
 
 export function DisplayState(props) {
-  return <div></div>
+    const classes = [];
+    console.log(props.classes)
+    for (let crn in props.classes) {
+        if (props.classes[crn] == true) {
+            classes.push(crn);
+        }
+    }
+    return (<div>
+    {classes.map((crn) => {
+        return crn;
+    })}
+    </div>);
 }
 
 export function CheckboxClass(props) {
