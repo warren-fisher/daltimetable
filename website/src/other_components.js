1814ad393636a8a50dc357b780f28bac714cb948
Warren Fisher <github@warrenfisher.dev>
2020-04-20 15:22:22 -0300

    each class has its own working checkbox

---
+++ website/src/other_components.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+export function CheckboxDay(props) {
+    return (
+        <div className='day-checkbox'>
+            <input type='checkbox' name={props.day} onChange={props.handleChange} checked={props.checked}/>
+            <label htmlFor={props.day}>{props.day}</label>
+        </div>
+    );
+}
+
+export function ClassInfo(props) {
+  const d = props.data;
+  return (
+    <div className="class_" id={d.crn}>
+        <input type='checkbox' name={d.crn} onChange={props.handleChange} checked={props.checked}/>
+        CRN={d.crn} dept={d.department} name={d.name} start={d.start_time} end={d.end_time} days={d.dates}
+    </div>
+  );
+}
+
+export function DisplayState(props) {
+  return <div></div>
+}
+
+export function CheckboxClass(props) {
+
+}
