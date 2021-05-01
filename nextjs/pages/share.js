a06ceac8124d691caf8b27cee448a917f96bdc80
Warren Fisher <warrenfisher@dal.ca>
2021-05-01 15:00:28 -0300

    Update accessibility with more labels remove accidental file

--- nextjs/pages/share.js
+++
@@ -1,8 +0,0 @@
-<Route path="/share/:id" children={
-    <RenderTable
-        width={this.state.size.width}
-        height={this.state.size.height}
-        getTermState={this.getTermState}
-        handleChange={this.handleChange}
-        terms={this.state.termsSelected} />
-} />
